1bc75ed8cb286184e96d7fe606ebeba319814a5d
arcilac <arcilacamila08@gmail.com>
2024-04-08 18:41:35 -0500

    add existing file

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,14 +1,31 @@
 import React from "react";
+// import './UseStates.css';
 
 class ClassState extends React.Component{
+
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            error: true,
+        };
+    }
+
     render() {
         return (
             <div>
-                <h2>Delete ClassState</h2>
+                <h2>Delete {this.props.name}</h2>
                 <p>type the security code to check that you want to delete </p>
 
+                {this.state.error && (
+            <p>Error: the code is wrong </p>
+        )}
                 <input placeholder="security code" />
-                <button>Check</button>
+                <button
+                    onClick={() => 
+                        this.setState(prevState => ({error: !prevState.error  }))
+                    }
+                >Check</button>
             </div>
         )
     }
--- src/UseState.js
+++ src/UseState.js
@@ -1,13 +1,21 @@
 import React from "react";
+import './UseStates.css';
 
-function UseState() {
+function UseState({ name }) {
+    const [error, setError] = React.useState(true);
     return (
         <div>
-        <h2>Delete UseState</h2>
+        <h2>Delete {name}</h2>
         <p>type the security code to check that you want to delete </p>
 
+        {error && (
+            <p>Error: the code is wrong </p>
+        )}
+
         <input placeholder="security code" />
-        <button>Check</button>
+        <button
+        onClick={() => setError(!error)}
+        >Check</button>
     </div>
     )
 }
